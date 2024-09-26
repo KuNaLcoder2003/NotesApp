caa9c0240be47fc300bad1f66b63d09952cba348
Kunal Singh <kunalindia59@gmail.com>
2024-09-26 23:55:11 +0530

    added a route handler for getting a specific batch with given Id from frontend

---
+++ backend/routes/batch.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router();
+const {Student,Teacher,Batch,Notes } = require('../db')
+const jwt = require('jsonwebtoken')
+const key = require('../secret');
+const jwt_key = key.jwt_key
+const {signup , signin} = require('../types')
+const authMiddleWare = require('../middlewares/authMiddleware');
+const { default: mongoose } = require('mongoose');
+
+
+router.get('/:Id'  , async(req, res)=> {
+    const batchId = req.params.Id
+    try {
+        const batch = await Batch.findOne({_id : batchId}).populate('teacher');
+        if(!batch) {
+            return res.json({
+                messsage : "No course found with id : " + batchId
+            })
+        }
+        
+        res.status(200).json({
+            batch : batch
+        })
+    } catch (error) {
+        res.status(500).json({
+            message : "Something went wrong"
+        })
+    }
+})
+
+module.exports = router
