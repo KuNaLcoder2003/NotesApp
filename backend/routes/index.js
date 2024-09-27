c57d8aa94595423bafef0c53b928b047a6943f39
Kunal Singh <kunalindia59@gmail.com>
2024-09-27 23:08:52 +0530

    added a route for all /notes requests

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const router = express.Router();
 const student = require('./student')
 const teacher = require('./teacher')
+const batch = require('./batch')
+const notes = require('./notes')
 
 router.use('/student' , student)
 router.use('/teacher' , teacher)
+router.use('/batch' , batch)
+router.use('/notes' , notes)
+
 
 module.exports = router
