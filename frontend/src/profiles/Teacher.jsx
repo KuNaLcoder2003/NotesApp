6bd218920444871ffd8f9e9c145fe1e977a72009
Kunal Singh <kunalindia59@gmail.com>
2024-09-28 19:36:14 +0530

    Added signup/signin for teachers

--- frontend/src/profiles/Teacher.jsx
+++ frontend/src/profiles/Teacher.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 
-const Teacher = () => {
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+// import Header from '../header/Header';
+// import Batches from '../batches/Batches';
+import SignUp from '../components/signup/SignUp'
+import SignIn from '../components/signin/SignIn';
+
+const Teacher = ({setLogin}) => {
   return (
-    <div>
-      Teacher
+
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" , gap : "2rem" , height : "100vh" }}>
+      <SignUp url={'http://localhost:3000/api/v1/teacher/signup'} setLogin={setLogin} type= {"teacher"} />
+      <SignIn url={'http://localhost:3000/api/v1/teacher/signin'} setLogin={setLogin} type ={"teacher"} />
     </div>
+
   )
 }
 
