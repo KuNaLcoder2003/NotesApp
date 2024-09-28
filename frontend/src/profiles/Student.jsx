3986b499c6095839aa292b40f871010d3dafdef2
Kunal Singh <kunalindia59@gmail.com>
2024-09-28 19:35:14 +0530

    minor changes

--- frontend/src/profiles/Student.jsx
+++ frontend/src/profiles/Student.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import SignUp from '../components/signup/SignUp'
 import SignIn from '../components/signin/SignIn';
-import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
-import StudentDashBoard from '../components/StudentDashBoard/StudentDashBoard';
 
 const Student = ({setLogin}) => {
 
   return (
-    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
-      <SignUp url={'http://localhost:3000/api/v1/student/signup'} setLogin={setLogin}  />
-      <SignIn url={'http://localhost:3000/api/v1/student/signin'} setLogin={setLogin}  />
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" , height : "100vh" }}>
+      <SignUp url={'http://localhost:3000/api/v1/student/signup'} setLogin={setLogin} type={"student"} />
+      <SignIn url={'http://localhost:3000/api/v1/student/signin'} setLogin={setLogin} type={"student"}  />
     </div>
   )
 }
