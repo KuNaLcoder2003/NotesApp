7d0018baeb6e5c6c1445be3a23a09adbe68b0f58
Kunal Singh <kunalindia59@gmail.com>
2024-10-02 22:56:43 +0530

    added an endpoint for student to update their details

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,205 +1,224 @@
 const express = require('express')
-const {Student,Teacher,Batch,Notes } = require('../db')
+const { Student, Teacher, Batch, Notes } = require('../db')
 const jwt = require('jsonwebtoken')
 const key = require('../secret');
 const jwt_key = key.jwt_key
-const {signup , signin} = require('../types')
+const { signup, signin } = require('../types')
 const authMiddleWare = require('../middlewares/authMiddleware');
 const { default: mongoose } = require('mongoose');
 
 
 const router = express.Router()
 
-router.post('/signup' , async(req,res) => {
-    const { first_name , last_name , username , password ,age } = req.body;
+router.post('/signup', async (req, res) => {
+    const { first_name, last_name, username, password, age } = req.body;
     try {
         const { success } = signup.safeParse(req.body)
-        if(!success){
+        if (!success) {
             return res.status(400).json({
-                message : "Invalid Input type"
+                message: "Invalid Input type"
             })
         }
-        const student = await Student.findOne({username : username});
-        if(student) {
+        const student = await Student.findOne({ username: username });
+        if (student) {
             return res.status(401).json({
-                message : 'student already exists'
+                message: 'student already exists'
             })
         }
         const new_Student = new Student({
-            first_name : first_name,
-            last_name : last_name,
-            username : username,
-            age : age,
-            password : password,
+            first_name: first_name,
+            last_name: last_name,
+            username: username,
+            age: age,
+            password: password,
         })
         await new_Student.save()
         const studentId = new_Student._id
-        const token = jwt.sign({userId : studentId } , jwt_key)
+        const token = jwt.sign({ userId: studentId }, jwt_key)
         res.status(200).json({
-            message : "Account created",
-            token : token,
-            id : new_Student._id
+            message: "Account created",
+            token: token,
+            id: new_Student._id
         })
     } catch (error) {
         res.status(500).json({
-            message : "Something went wrong"
+            message: "Something went wrong"
         })
     }
 })
 
-router.post('/signin' , async(req,res)=>{
-    const {username , password} = req.body;
+router.post('/signin', async (req, res) => {
+    const { username, password } = req.body;
     try {
-        const {success} = signin.safeParse(req.body);
-        if(!success) {
+        const { success } = signin.safeParse(req.body);
+        if (!success) {
             return res.status(400).json({
-                message : "Invalid Input type"
+                message: "Invalid Input type"
             })
         }
-        const student = await Student.findOne({username : username , password : password});
-        if(!student){
+        const student = await Student.findOne({ username: username, password: password });
+        if (!student) {
             return res.status(401).json({
-                message : "User not found"
+                message: "User not found"
             })
         }
-        const token = jwt.sign({userId : student._id} , jwt_key)
+        const token = jwt.sign({ userId: student._id }, jwt_key)
         res.status(200).json({
-            message : "Successfully logged in",
-            token : token,
-            id : student._id
+            message: "Successfully logged in",
+            token: token,
+            id: student._id
         })
     } catch (error) {
         res.status(500).json({
-            message : "Something went wrong"
+            message: "Something went wrong"
         })
     }
 })
 
-router.get('/batches' , authMiddleWare , async(req,res)=> {
+router.get('/batches', authMiddleWare, async (req, res) => {
     // const userId = req.userId
     try {
         const batches = await Batch.find({}).populate('teacher')
-        if(batches.length == 0) {
+        if (batches.length == 0) {
             return res.json({
-                message : "Currently no batches"
+                message: "Currently no batches"
             })
         }
         res.status(200).json({
-            batches : batches.map( batch => ({
-                batch_id : batch._id,
-                batch_name : batch.batch_name,
-                teacher_name : batch.teacher.first_name,
-                teacher_username : batch.teacher.username
+            batches: batches.map(batch => ({
+                batch_id: batch._id,
+                batch_name: batch.batch_name,
+                teacher_name: batch.teacher.first_name,
+                teacher_username: batch.teacher.username
             }))
         })
 
     } catch (error) {
         res.status(500).json({
-            message : "something went wrong"
+            message: "something went wrong"
         })
     }
 })
 
-router.post('/purchase/:batchId' , authMiddleWare , async(req,res)=> {
+router.post('/purchase/:batchId', authMiddleWare, async (req, res) => {
     // transactions
     const studentId = req.userId;
     const batchId = req.params.batchId;
     try {
 
         // first see if student is already enrolled
-        const purchased = await Student.findOne({_id : studentId , batches : batchId});
-        if(purchased) {
+        const purchased = await Student.findOne({ _id: studentId, batches: batchId });
+        if (purchased) {
             return res.status(401).json({
-                valid : false,
-                message : "you are already enrolled in this course"
+                valid: false,
+                message: "you are already enrolled in this course"
             })
-        }        
-        const student = await Student.findOneAndUpdate({_id : studentId} , {$push : {batches : batchId}})
-        const batch = await Batch.findOneAndUpdate({_id : batchId} , {$push : {students : studentId}})
+        }
+        const student = await Student.findOneAndUpdate({ _id: studentId }, { $push: { batches: batchId } })
+        const batch = await Batch.findOneAndUpdate({ _id: batchId }, { $push: { students: studentId } })
         res.status(200).json({
-            valid : true,
-            message : "Added Successfully"
+            valid: true,
+            message: "Added Successfully"
         })
     } catch (error) {
         res.status(500).json({
-            message : "something went wrong"
+            message: "something went wrong"
         })
     }
 })
 
-router.get('/purchased' ,  authMiddleWare , async(req,res)=> {
+router.get('/purchased', authMiddleWare, async (req, res) => {
     const studentId = req.userId;
     try {
-        const student= await Student.findOne({_id : studentId}).populate('batches');
-        if(student.length == 0){
+        const student = await Student.findOne({ _id: studentId }).populate('batches');
+        if (student.length == 0) {
             return res.json({
-                message : "No purchased Batches"
+                message: "No purchased Batches"
             })
         }
         // console.log(student.batches)
         res.status(200).json({
-            batches : student.batches,
-            
+            batches: student.batches,
+
         })
     } catch (error) {
         res.status(500).json({
-            message : "Error fetching Courses"
+            message: "Error fetching Courses"
         })
     }
 })
 
 
-router.get('/content/:batchId' , authMiddleWare , async(req,res)=> {
+router.get('/content/:batchId', authMiddleWare, async (req, res) => {
     const userId = req.userId;
     const batchId = req.params.batchId;
     try {
-        const access = await Student.findOne({ _id : userId ,batches : batchId })
-        if(!access){
+        const access = await Student.findOne({ _id: userId, batches: batchId })
+        if (!access) {
             return res.status(401).json({
-                valid : false,
-                message : "Purchase the course to view content"
+                valid: false,
+                message: "Purchase the course to view content"
             })
         }
-        const batch = await Batch.findOne({_id : batchId}).populate('teacher')
+        const batch = await Batch.findOne({ _id: batchId }).populate('teacher')
         res.status(200).json({
-            valid : true,
-            userId : userId,
-            batch : batch,
-            message : "You have the access"
+            valid: true,
+            userId: userId,
+            batch: batch,
+            message: "You have the access"
 
         })
     } catch (error) {
         res.status(500).json({
-            message : "something went wrong"
+            message: "something went wrong"
         })
     }
 })
 
-router.get('/detail' ,authMiddleWare , async(req,res)=>{
+router.get('/detail', authMiddleWare, async (req, res) => {
     const studentId = req.userId;
     try {
-        const student = await Student.findOne({_id : studentId}).populate('batches')
-        if(!student){
+        const student = await Student.findOne({ _id: studentId }).populate('batches')
+        if (!student) {
             return res.status(400).json({
-                valid : false,
-                message : 'Error getting detials'
+                valid: false,
+                message: 'Error getting detials'
             })
         }
         res.status(200).json({
-            valid : true,
-            student_name : `${student.first_name} ${student.last_name}`,
-            username : student.username,
-            password : student.password,
-            batches : student.batches,
-            age : student.age
+            valid: true,
+            student_name: `${student.first_name} ${student.last_name}`,
+            username: student.username,
+            password: student.password,
+            batches: student.batches,
+            age: student.age
         })
     } catch (error) {
         res.status(500).json({
-            valid : false , 
-            message : "Something went wrong"
+            valid: false,
+            message: "Something went wrong"
         })
     }
-} )
+})
+
+router.put('/edit', authMiddleWare, async (req, res) => {
+    const studentId = req.userId;
+    const edited = req.body
+    try {
+        const student = await Student.findOneAndUpdate({ _id: studentId }, edited, { new: true, runValidators: true })
+        if (!student) {
+            return res.status(404).json({ message: "Student not found", valid: false });
+        }
+        res.status(200).json({
+            valid: true,
+            message: "Updated student details"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message : "Error updating student details"
+        })
+    }
+})
 
 
 module.exports = router
