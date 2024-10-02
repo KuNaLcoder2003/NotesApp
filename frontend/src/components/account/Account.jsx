0b264d2d2e2894a0f590c2b2b278286ef78c5e61
Kunal Singh <kunalindia59@gmail.com>
2024-10-02 23:26:31 +0530

    added an edit button with a new component , which sends edit request to update deatils of user based on prop url

--- frontend/src/components/account/Account.jsx
+++ frontend/src/components/account/Account.jsx
@@ -2,6 +2,80 @@ import React, { useEffect, useState } from 'react'
 import "./account.css"
 import { useNavigate } from 'react-router-dom'
 
+
+const Edit = ({usersName , username , password , url ,setEdit })=> {
+    const [editedData , setEditedData] = useState({
+        usersName : usersName,
+        username : username,
+        password : password,
+    })
+
+    const submitHandler = (e)=> {
+        e.preventDefault()
+        let obj = {};
+        const token = localStorage.getItem('token');
+        try {
+            if(editedData.usersName !== usersName) {
+                let arr = editedData.usersName.split(' ')
+                let first_name = arr[0]
+                let last_name = arr[1]
+                obj.first_name = first_name;
+                obj.last_name = last_name
+            }
+            else if( editedData.username !== username){
+                obj.username = editedData.username
+            }
+            else if (editedData.password !== password) {
+                obj.password = editedData.password
+                
+            }
+            else {
+                return ;
+            }
+            console.log('updating')
+            fetch(url , {
+                method : 'PUT',
+                body : JSON.stringify({
+                    ...obj
+                }),
+                headers : {
+                    'Content-Type' : 'application/json',
+                    authorization : token
+                },
+            }).then(async(data)=>{
+                const response = await data.json()
+                console.log(response)
+            })
+            
+        } catch (error) {
+            
+        }
+    }
+
+    return ( 
+        <form onSubmit={submitHandler}>
+            <div>
+                <p>Name : </p>
+                <input type='text' value={editedData.usersName} onChange={(e)=> setEditedData({...editedData , usersName : e.target.value})}/>
+            </div>
+            <br/>
+            <div>
+                <p>Username : </p>
+                <input type='text' value={editedData.username} onChange={(e)=>setEditedData({...editedData , username : e.target.value})} />
+
+            </div>
+            <br/>
+
+            <div>
+                <p>Password : </p>
+                <input type='text' value={editedData.password} onChange={(e)=>setEditedData({...editedData , password : e.target.value})} />
+            </div>
+
+            <button>Edit</button>
+
+        </form>
+    )
+}
 const Account = ({url , type}) => {
     const [accountData , setAccountData] = useState({
         usersName : "",
@@ -9,6 +83,7 @@ const Account = ({url , type}) => {
         password : "",
         batches : []
     })
+    const [edit , setEdit] = useState(false)
     const navigate = useNavigate();
     useEffect(()=>{
         const token = localStorage.getItem('token')
@@ -71,6 +146,12 @@ const Account = ({url , type}) => {
             </>)
         }
       </div>
+      <button onClick={()=>setEdit(!edit)}>Edit details</button>
+      {
+        edit ? (<Edit usersName={accountData.usersName} username={accountData.username} password={accountData.password} setEdit={setEdit}
+            url={type == "student" ? 'http://localhost:3000/api/v1/student/edit' : 'http://localhost:3000/api/v1/teacher/edit'} />) : 
+            (null)
+      }
     </div>
   )
 }
